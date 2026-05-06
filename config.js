1a3ee8ac5c04ee09650a238e37507265774db8b1
Gustavo Voltarel <gustavo.voltarel@santacasasaocarlos.com.br>
2026-05-06 12:08:27 -0300

    supabase

---
+++ config.js
@@ -0,0 +1,10 @@
+/*
+ * Configurações do projeto.
+ * Este arquivo precisa ficar no mesmo diretório dos arquivos HTML.
+ */
+
+window.SUPABASE_URL = "https://ztrzwaosybnmzwjufqld.supabase.co";
+
+window.SUPABASE_KEY = "sb_publishable_oKFgxOaFvi21vRGpymMKOg_Z26OATE6";
+
+window.ADMIN_EMAILS = ["g.voltarel95@gmail.com"];
